7fb04b3a8c6d24cdb19fb77243d18538d2c24364
Gaurav Mukherjee <gauravmukherjee69@gmail.com>
2024-10-02 00:32:12 +0530

    Enhance game:
    - Add welcome art
    - Implement score feature
    - Improve play again choice

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -15,20 +15,60 @@ def get_computer_choice():
 
 def determine_winner(user, computer):
     if user == computer:
-        return "It's a tie!"
-    if (user == "rock" and computer == "scissors") or (user == "paper" and computer == "rock") or (user == "scissors" and computer == "paper"):
-        return "You win!"
-    return "Computer wins!"
+        print("It's a tie")
+        return 0
+    
+    elif (user == "rock" and computer == "scissors") or (user == "paper" and computer == "rock") or (user == "scissors" and computer == "paper"):
+        print("You win! Congratulations!")
+        return 1
+    
+    else:
+        print("Computer wins! Better luck next time!")
+        return -1
 
+print("\nWelcome to\n") 
+print(r"  ___         _               ")
+print(r" | _ \___  __| |__            ")
+print(r" |   / _ \/ _| / /            ")
+print(r" |_|_\___/\__|_\_\            ")
+print(r" | _ \__ _ _ __  ___ _ _      ")
+print(r" |  _/ _` | '_ \/ -_) '_|     ")
+print(r" |_| \__,_| .__/\___|_|       ")
+print(r"  ___     |_|                 ")
+print(r" / __| __(_)______ ___ _ _ ___")
+print(r" \__ \/ _| (_-<_-</ _ \ '_(_-<")
+print(r" |___/\__|_/__/__/\___/_| /__/")
+print("\n")
 
-while True:
-    user_choice = get_user_choice()
-    computer_choice = get_computer_choice()
-    print(f"You chose {user_choice}")
-    print(f"Computer chose {computer_choice}")
-    result = determine_winner(user_choice, computer_choice)
-    print(result)
+def main():
+    user_score = 0
+    computer_score = 0
 
-    play_again = input("Do you want to play again? (yes/no): ").strip().lower()
-    if play_again != "yes":
-        break
+    while True:
+        user_choice = get_user_choice()
+        computer_choice = get_computer_choice()
+        print(f"You chose {user_choice}")
+        print(f"Computer chose {computer_choice}")
+        result = determine_winner(user_choice, computer_choice)
+
+        if result == 1:
+            user_score += 1
+        
+        elif result == -1:
+            computer_score += 1
+
+        play_again = input("Do you want to play again? (yes/no): ").strip().lower()
+
+        if play_again in ["yes", "y"]:
+            continue
+
+        elif play_again in ["no", "n"]:
+            print(f"Score - You: {user_score}, Computer: {computer_score}")
+            print("Thank you for playing!")
+            break
+
+        else:
+            print("Error - Invalid input!")
+
+if __name__ == "__main__":
+    main()
