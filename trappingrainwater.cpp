47115eab5e39ad7dbd31ce72978547dd8b5b8ca6
sachinggsingh <152975718+sachinggsingh@users.noreply.github.com>
2024-10-02 11:47:38 +0530

    minor things like header files have been changed

--- trappingrainwater.cpp
+++ trappingrainwater.cpp
@@ -1,4 +1,8 @@
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+#include <iostream>
+//for using the cout and cin in c++
+#include <vector>
+// Instead of including bits/stdc++.h, you can include the specific header files that you need for your program. For example, if you're using vectors, you can include vector instead
 using namespace std;
 
 int trap(vector<int>& height) {
@@ -27,19 +31,27 @@ int trap(vector<int>& height) {
 }
 
 int main() {
-    vector<int> arr;
+    // vector<int> arr;
     int n;
     cout << "Enter the number of elements: ";
     cin >> n;
+    if(n <= 0 )
+    {
+        cout << "Invalid Input";
+        return 1;
+    }
 
+    vector<int> arr;
     cout << "Enter the height of each bar: ";
     for (int i = 0; i < n; i++) {
         int height;
         cin >> height;
-        arr.push_back(height);
+        arr.push_back(height); 
     }
-
-    cout << "The water that can be trapped is " << trap(arr) << endl;
+    
+    int waterIn = trap(arr);
+    cout << "The water that can be trapped is " << waterIn << endl;
+    // cout << "The water that can be trapped is " << trap(arr) << endl;
 
     return 0;
 }
