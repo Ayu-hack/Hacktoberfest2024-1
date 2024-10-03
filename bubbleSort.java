97368e906076d6528b3dcd9495b2aab8da501d6a
Suvojit Modak <47207810+Suvoji01@users.noreply.github.com>
2024-10-03 23:23:23 +0530

    Create bubbleSort.java
    Bubble Sort (Sorting Algorithm)

---
+++ bubbleSort.java
@@ -0,0 +1,29 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 4, 2, 8};
+        bubbleSort(arr);
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+
+    static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
