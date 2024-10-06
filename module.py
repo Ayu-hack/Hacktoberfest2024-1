c49fb47291abb614810bbe7c31fb7d477eb7e41e
Encrypter.404 <155004817+Encrypter-404@users.noreply.github.com>
2024-10-06 21:30:37 +0530

    Create module.py

---
+++ module.py
@@ -0,0 +1,11 @@
+# importing sympy module
+from sympy import *
+
+# calling isprime function on different numbers
+geek1 = isprime(30)
+geek2 = isprime(13)
+geek3 = isprime(2)
+
+print(geek1) # check for 30 is prime or not
+print(geek2) # check for 13 is prime or not
+print(geek3) # check for 2 is prime or not
