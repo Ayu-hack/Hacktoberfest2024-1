a9ed0d6cd0d77f382cdfb009e2c987cbd920302b
Kajal Ahirwar <kajalahirwar13032003@gmail.com>
2024-10-08 11:27:45 +0530

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Strings {
+    public static void main(String args[]) {
+        String sentence = "My name is kajal";
+
+        String name = sentence.substring((10));
+        System.out.println(name);
+
+    }
+}
