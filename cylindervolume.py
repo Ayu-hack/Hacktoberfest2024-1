31e2428ea1e2e2a8a9252945e5be8a7956b48f0e
Thambapanni-Designs <116666202+Thambapanni-Designs@users.noreply.github.com>
2024-10-15 22:04:32 +0530

    Create cylindervolume.py

---
+++ cylindervolume.py
@@ -0,0 +1,13 @@
+#Initializing variables
+volume = 0
+radius =0
+height =0
+
+#input
+radius = float(input("Enter the radius: "))
+height = float(input("Enter the height: "))
+
+#operation
+volume = (22/7)*(radius**2)*height
+
+print("Volume of the cylinder is: ",volume)
