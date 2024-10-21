5b61cc733aa27e79e52d189534f4dcf12cff49a8
pandrew5chu <132870412+pandrew5chu@users.noreply.github.com>
2024-10-21 15:22:11 -0700

    Create addition.generator.py
    This addition generator gets 2 numbers and adds them together and prints out the sum. This is used to help people do addition by adding the 2 numbers they put in.

---
+++ CrossPix/addition.generator.py
@@ -0,0 +1,9 @@
+def addition_generator(num1, num2):
+    return num1 + num2
+
+# Example usage
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+result = addition_generator(number1, number2)
+print(f"The sum of {number1} and {number2} is {result}")
